da632169e4487a8a1cd456b63837b1bfbeb714a2
Sonok Mahapatra <sonokmahapatra@gmail.com>
2026-03-07 18:10:32 -0500

    kth largest

--- 09_heap/practice.py
+++ 09_heap/practice.py
@@ -43,7 +43,12 @@ from collections import Counter
 #   Output: 5
 # ---------------------------------------------------------------------------
 def find_kth_largest(nums: list[int], k: int) -> int:
-    pass  # YOUR CODE HERE
+    heap = []
+    for num in nums:
+        heapq.heappush(heap, -num)
+    for _ in range(k-1):
+        heapq.heappop(heap)
+    return -heap[0]
 
 
 # ---------------------------------------------------------------------------
